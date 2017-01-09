32267e3132c599cba1a226319b030017da035b84
Mai Truong <maitruong2801@gmail.com>
2017-01-09 14:06:12 -0800

    add steps attribute to Tally

--- app/src/main/java/com/android/mqtruong/expensessaver/Tally.java
+++ app/src/main/java/com/android/mqtruong/expensessaver/Tally.java
@@ -6,8 +6,12 @@ import android.widget.LinearLayout;
 public class Tally {// extends LinearLayout{
     int value;
     String name;
-    public Tally(String name, int value) {
+    double amount;
+    double steps;
+    public Tally(String name, int value, double amount, double steps) {
         this.name = name;
         this.value = value;
+        this.amount = amount;
+        this.steps = steps;
     }
 }
