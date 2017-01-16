6e54ed8c476009c068e4b87be1219b507865dda2
Mai Truong <maitruong2801@gmail.com>
2017-01-15 21:55:22 -0800

    remove unused imports

--- app/src/main/java/com/android/mqtruong/expensessaver/AddDialog.java
+++ app/src/main/java/com/android/mqtruong/expensessaver/AddDialog.java
@@ -2,18 +2,14 @@ package com.android.mqtruong.expensessaver;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.FragmentManager;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.widget.DrawerLayout;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import java.util.ArrayList;
 
 public class AddDialog extends DialogFragment {
     public static final String TAG = "AddDialog";
