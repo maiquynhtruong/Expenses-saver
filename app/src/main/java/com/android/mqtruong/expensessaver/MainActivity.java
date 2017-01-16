aa3498ef8de6b7fb415627c163258a5a2d183422
Mai Truong <maitruong2801@gmail.com>
2017-01-15 17:57:25 -0800

    add save tallies, get tallies, clear tallies, show dialog, and other funcitonalities

--- app/src/main/java/com/android/mqtruong/expensessaver/MainActivity.java
+++ app/src/main/java/com/android/mqtruong/expensessaver/MainActivity.java
@@ -1,22 +1,26 @@
 package com.android.mqtruong.expensessaver;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    MainApplication application;
+    SharedPreferences sharedPreferences;
     ArrayList<Tally> tallyList;
-    int count = 0;
+    TallyAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,24 +32,78 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                addTally();
+                createDialog();
             }
         });
-        getTallies();
-        application.addTally("First Count", 0);
-        application.addTally("Second Count", 1);
-        Log.d("MainActivity.count", count++ + "");
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        tallyList = new ArrayList<>();
+
         ListView tallyView = (ListView) findViewById(R.id.tally_list_view);
-        TallyAdapter adapter = new TallyAdapter(this, tallyList);
+        adapter = new TallyAdapter(this, tallyList);
         tallyView.setAdapter(adapter);
     }
 
-    private void getTallies() {
-        application = new MainApplication();
-        tallyList = application.tallyArrayList;
+    private void createDialog(){
+        AddDialog dialog = new AddDialog();
+        // show the dialog
+        dialog.show(getSupportFragmentManager(), AddDialog.TAG);
     }
+
+    public void getTallies() {
+        Map<String, ?> dataMap = sharedPreferences.getAll();
+
+        if (tallyList.isEmpty() || dataMap.isEmpty()) {
+            addTally(new Tally("First Tally", Tally.DEFAULT_VALUE, Tally.DEFAULT_AMOUNT, Tally.DEFAULT_STEP));
+        } else {
+            for (String name : dataMap.keySet()) {
+                int value = (Integer) dataMap.get(name + "_value");
+                double amount = (Double) dataMap.get(name + "_amount");
+                double step = (Double) dataMap.get(name + "_step");
+                tallyList.add(new Tally(name, value, amount, step));
+            }
+        }
+    }
+
+    public void addTally(Tally tally) {
+        tallyList.add(tally);
+        adapter.notifyDataSetChanged();
+    }
+
+    public void saveTallies() {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        for (Tally tally : tallyList) {
+            editor.putInt(tally.name + "_value", tally.value);
+            editor.putFloat(tally.name + "_amount", (float) tally.amount);
+            editor.putFloat(tally.name + "_step", (float) tally.steps);
+        }
+        editor.commit();
+    }
+
+    public void removeAllTallies() {
+        tallyList.clear();
+        addTally(new Tally("First Tally", Tally.DEFAULT_VALUE, Tally.DEFAULT_AMOUNT, Tally.DEFAULT_STEP));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getTallies();
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveTallies();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -68,8 +126,6 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void addTally() {
-        // Show a dialog box and call addTally in MainApplication
-    }
+
 
 }
