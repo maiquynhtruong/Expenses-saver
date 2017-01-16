0a59c9c5a05a394f9bd0bdbdb5a09459c841bd46
Mai Truong <maitruong2801@gmail.com>
2017-01-16 08:07:38 -0800

    add layout for edit dialog

---
+++ app/src/main/java/com/android/mqtruong/expensessaver/EditDialog.java
@@ -0,0 +1,14 @@
+package com.android.mqtruong.expensessaver;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+
+public class EditDialog extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return super.onCreateDialog(savedInstanceState);
+    }
+}
