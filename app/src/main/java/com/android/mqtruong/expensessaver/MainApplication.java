dd729d760d0b0d4f4299e9c8b596991dd9834ffb
Mai Truong <maitruong2801@gmail.com>
2017-02-04 21:33:02 -0800

    call the app rater from main application

--- app/src/main/java/com/android/mqtruong/expensessaver/MainApplication.java
+++ app/src/main/java/com/android/mqtruong/expensessaver/MainApplication.java
@@ -1,8 +1,16 @@
 package com.android.mqtruong.expensessaver;
 
+import android.app.Application;
+
 /**
  * Created by Mai on 2/4/2017.
  */
 
-public class MainApplication {
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AppRater.app_launched(this);
+    }
 }
